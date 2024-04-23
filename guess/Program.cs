ffd50b63bcd41152e9ff7bc9398e253ea5fa0079
omid <81793101+omidxrz@users.noreply.github.com>
2024-04-23 04:20:28 +0330

    .

---
+++ guess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101);
+        int wrongGuessCount = 0;
+
+        while (true)
+        {
+            Console.Write("Guess the number (between 1 and 100): ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int guess))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            if (guess < 1 || guess > 100)
+            {
+                Console.WriteLine("Please enter a number between 1 and 100.");
+                continue;
+            }
+
+            if (guess < secretNumber)
+            {
+                Console.WriteLine("The secret number is bigger.");
+                wrongGuessCount++;
+            }
+            else if (guess > secretNumber)
+            {
+                Console.WriteLine("The secret number is smaller.");
+                wrongGuessCount++;
+            }
+            else
+            {
+                Console.WriteLine("Nice Catch! You guessed the secret number: " + secretNumber);
+                Console.WriteLine("Number of wrong guesses: " + wrongGuessCount);
+                break;
+            }
+        }
+    }
+}
---
+++ login/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> usernames = new List<string>{"user1", "user2", "user3"};
+        List<string> passwords = new List<string>{"pass1", "pass2", "pass3"};
+        List<int> attemptsLeft = new List<int>{3, 3, 3};
+
+        bool loggedIn = false;
+
+        while (!loggedIn)
+        {
+            Console.Write("[+] Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("[+] Enter password: ");
+            string password = Console.ReadLine();
+
+            // Check if username and password are correct
+            int index = usernames.IndexOf(username);
+            if (index == -1){
+                Console.WriteLine("\n[-] Username not found!\n");
+                continue;
+            }
+            
+            if (attemptsLeft[index] <= 0)
+            {
+                Console.WriteLine($"\n[-] Your account {username} is blocked!!!!\n");
+                continue;
+            }
+
+            if (index != -1 && passwords[index] == password)
+            {
+                loggedIn = true;
+                Console.WriteLine("\nWelcome, " + username + "!\n");
+            }
+            else
+            {
+                Console.WriteLine("\n[-] Invalid password!\n");
+                // int userIndex = usernames.IndexOf(username);
+                if (index != -1)
+                {
+                    attemptsLeft[index]--;
+                    Console.WriteLine($"[*] You have {attemptsLeft[index]} attempts left for {username} !!\n");  
+                }else{
+                    Console.WriteLine("qwe");
+                }
+            }
+        }
+    }
+}
